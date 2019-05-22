e2a0fe492b14cb797db328ee79bfa5228c7503e0
Federico Buti <federico.buti@proemion.com>
2019-05-22 22:34:00 +0200

    Build latest mesa lib, independently of the chosen architecture

--- src/mesa.mk
+++ src/mesa.mk
@@ -1,13 +1,21 @@
 PKG             := mesa
-$(PKG)_VERSION  := 17.3.0
-$(PKG)_CHECKSUM := 29a0a3a6c39990d491a1a58ed5c692e596b3bfc6c01d0b45e0b787116c50c6d9
+$(PKG)_VERSION  := 19.0.5
+$(PKG)_CHECKSUM := 6aecb7f67c136768692fb3c33a54196186c6c4fcafab7973516a355e1a54f831
 $(PKG)_SUBDIR   := mesa-$($(PKG)_VERSION)
 $(PKG)_FILE     := mesa-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := ftp://ftp.freedesktop.org/pub/mesa/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc
 
+$(PKG)_TARGETS       := $(BUILD) $(MXE_TARGETS)
+
+MACHINE_ARCHITECTURE := x86
+
+ifeq ($(BITS), 64) 
+	MACHINE_ARCHITECTURE := $(MACHINE_ARCHITECTURE)_64
+endif
+
 define $(PKG)_BUILD
     cd '$(1)' && \
-    MINGW_PREFIX='$(TARGET)-' scons platform=windows toolchain=crossmingw machine=x86_64 verbose=1 build=release libgl-gdi
-    $(INSTALL) -m 755 '$(1)/build/windows-x86_64/gallium/targets/libgl-gdi/opengl32.dll' '$(PREFIX)/$(TARGET)/bin/'
+    MINGW_PREFIX='$(TARGET)-' scons platform=windows toolchain=crossmingw machine='$(MACHINE_ARCHITECTURE)' verbose=1 build=release libgl-gdi
+    $(INSTALL) -m 755 '$(1)/build/windows-'$(MACHINE_ARCHITECTURE)'/gallium/targets/libgl-gdi/opengl32.dll' '$(PREFIX)/$(TARGET)/bin/'
 endef
